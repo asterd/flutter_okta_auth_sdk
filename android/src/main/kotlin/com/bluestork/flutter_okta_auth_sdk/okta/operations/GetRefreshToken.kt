27b09387568e6070ed780abe6044c70b820618c1
christinealcachupas <30483218+christinealcachupas@users.noreply.github.com>
2023-07-12 13:50:12 +0800

    Create GetRefreshToken.kt
    Added getRegreshToken method

---
+++ android/src/main/kotlin/com/bluestork/flutter_okta_auth_sdk/okta/operations/GetRefreshToken.kt
@@ -0,0 +1,17 @@
+package com.bluestork.flutter_okta_auth_sdk.okta.operations
+
+import com.bluestork.flutter_okta_auth_sdk.okta.entities.Errors
+import com.bluestork.flutter_okta_auth_sdk.okta.entities.OktaClient
+import com.bluestork.flutter_okta_auth_sdk.okta.entities.PendingOperation
+
+fun getRefreshToken() {
+
+    var sessionClient = OktaClient.getWebClient().sessionClient
+    var tokens = sessionClient.tokens
+
+    if (tokens.refreshToken == null) {
+        PendingOperation.error(Errors.NO_ACCESS_TOKEN)
+    } else {
+        PendingOperation.success(tokens.refreshToken)
+    }
+}
